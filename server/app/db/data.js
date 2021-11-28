3bdc2eee8d65ca3f348aa8fe5ea7bdd3472f1ba5
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-11-28 11:49:50 -0600

    docs: :memo: create meals data

---
+++ server/app/db/data.js
@@ -0,0 +1,232 @@
+export default [
+    {
+        title: "Steak Peppercorn with Sauteed Carrots and French Green Beans",
+        id: 1,
+        ingredients: "Green beans, flat iron steak, red potatoes, carrots, cassava flour, plain yogurt",
+        description: `Tender, high-protein flat iron steak, grilled to perfection.
+        Bed of creamy mashed potatoes as a hearty accompaniment. 
+        Side of French green beans and vitamin-rich sautéed carrots tossed with garlic oil, lemon, and herbs. 
+        Topped with a creamy peppercorn sauce made with yogurt and porcini mushrooms. 
+        Try finishing your dish with freshly chopped parsley.`,
+        nutrition: "Calories: 500. Carbs: 38g. Total fat: 26g. Protein: 29g.",
+        diet: "regular"
+    },
+    {
+        title: "Homestyle Chicken with Masterful Mac and Cheese",
+        id: 2,
+        ingredients: "Butternut squash, cassava flour, elbow macaroni, green beans, chicken breast, almonds",
+        description: `Greaseless oven-fried chicken breast, hand-"breaded' with a gluten-free almond flour batter.
+        Our Masterful Mac is made with gluten-free brown-rice macaroni and a cheesy sauce thickened with butternut squash.
+        Garlicky green beans tossed in our fragrant, house-infused oil.
+        Add some chile kick with red pepper flakes or your favorite hot sauce.`,
+        nutrition: "Calories: 560. Carbs: 35g. Total fat: 27g. Protein: 46g.",
+        diet: "regular"
+    },
+    {
+        title: "Cauliflower Shell Beef Bolognese with Nonna's Soffritto and Italian Cheeses",
+        id: 3,
+        ingredients: "Celery, cauliflower, parmesan cheese, ground beef, carrots, marinara sauce",
+        description: `Our gluten- and grain-free cauliflower pasta shells and high-protein ground beef.
+        Our Masterful Marinara sauce is packed with five sneaky veggies and a hint of honey for sweetness.
+        Nonna knows best: our simmered Italian soffritto of carrots, onions, celery, garlic, and parsley builds rich flavor.
+        Topped with a blend of parmesan and asiago cheese.        
+        Our chefs recommend finishing your dish with freshly torn basil.`,
+        nutrition: "Calories: 490. Carbs: 49g. Total fat: 28g. Protein: 31g.",
+        diet: "regular"
+    },
+    {
+        title: "Sausage Baked Penne with Sauteed Zucchini and Spinach",
+        id: 4,
+        ingredients: "Italian sausage, spinach, zucchini, penne, tomatoes, marinara sauce",
+        description: `Pasta night gets an upgrade with gluten-free brown-rice pasta and our Masterful Marinara, packed with 5 sneaky veggies and a touch of honey.
+        Nitrate- and nitrite-free pork sausage with Italian spices.
+        Vitamin-rich veggies, including sauteed zucchini, grape tomatoes, and spinach.
+        Creamy blend of ricotta, mozzarella, and parmesan cheese.
+        Try topping with fresh basil, some extra grated cheese, and freshly cracked pepper.`,
+        nutrition: "Calories: 470. Carbs: 41g. Total fat: 31g. Protein: 20g.",
+        diet: "regular"
+    },
+    {
+        title: "Savory-Sweet Chicken Teriyaki Bowl with Basmati Rice and Veggies",
+        id: 5,
+        ingredients: "Green chickpeas, teriyaki sauce, carrots, coconut nectar, basmati rice, chicken tight",
+        description: `The salty-sweet flavors of chicken teriyaki just got a little better-for-you.
+        Juicy, chopped chicken thigh in a tantalizing sauce that’s just as craveable as the original, but sweetened naturally with coconut sap.
+        Tossed with matchstick carrots, green chickpeas, broccoli, bell peppers, and mushrooms.
+        A filling and fragrant base of basmati rice.`,
+        nutrition: "Calories: 480. Carbs: 66g. Total fat: 11g. Protein: 25g.",
+        diet: "regular"
+    },
+    {
+        title: "Coconut Lemongrass Pork Bowl with Cauliflower Rice and Fire-roasted Veggies",
+        id: 6,
+        ingredients: "Cauliflower, coconut milk, diced pork, garlic, ginger, lemongrass.",
+        description: `Power up and power through with a bowl of juicy, protein-packed pork.
+        Get a fix of colorful fiber from fire-roasted cauliflower, bell peppers, red onions, and yellow squash.
+        Our Thai-inspired, naturally sweetened (and dairy-free) lemongrass sauce is made with coconut milk and spiced with ginger and garlic.
+        Served on a bed of carb-conscious cauliflower rice.`,
+        nutrition: "Calories: 400. Carbs: 19g. Total fat: 27g. Protein: 22g.",
+        diet: "low-calorie"
+    },
+    {
+        title: "Baja Pork Bowl with Chile Verde and Summer Veggie Medley",
+        id: 7,
+        ingredients: "Brown rice, cauliflower, diced pork, onion, tomatillo, wild rice",
+        description: `We take juicy diced pork, cook it sous-vide until just right, and then sear it for even more flavor.
+        Mixed with sunny squash and three types of fire-roasted veg: red onions, cauliflower, and red bell peppers.
+        Our tangy green chile sauce is made of tomatillos and roasted poblanos, and brightened up with lime.
+        Served over a multicolored blend of wild and brown rice.`,
+        nutrition: "Calories: 420. Carbs: 46g. Total fat: 18g. Protein: 17g.",
+        diet: "regular"
+    },
+    {
+        title: "Hearty Chipotle-Chicken Stew with Black Beans and Monterey Jack",
+        id: 8,
+        ingredients: "Onion, black beans, tomatoes, corn, red bell pepper, chicken breast",
+        description: `Southwestern inspired and satisfyingly chunky—our nourishing go-to, no matter the hour.
+        Studded with chopped chicken breast, long-grain rice, corn, and black beans.
+        Jalapeño peppers and a hearty tap of chipotle powder add a kick of fiery heat.
+        A full-flavored base of diced tomatoes and spiced chicken broth, thickened with a purée of red bell peppers and onions.
+        Topped with smooth and nutty Monterey Jack.`,
+        nutrition: "Calories: 420. Carbs: 26g. Total fat: 23g. Protein: 24g.",
+        diet: "regular"
+    },
+    {
+        title: "Chicken Tikka Masala with Seven-Spice Vegetable Biryani",
+        id: 9,
+        ingredients: "Carrots, greek yogurt, peas, chickpeas, green beans, chicken tight",
+        description: `Get your spoon ready for our creamy yet virtuous masala sauce, slow-stirred with tomato, Greek yogurt, and warming spices.
+        Mixed with juicy chicken thighs and chickpeas for a hit of protein and fiber.
+        Served with a vegetable biryani of basmati rice, diced carrots, green beans, peas, and seven fragrant spices.
+        For a refreshing twist, try topping your dish with a homemade raita of Greek yogurt, sliced cucumber, and chopped mint.`,
+        nutrition: "Calories: 570. Carbs: 48g. Total fat: 28g. Protein: 27g.",
+        diet: "regular"
+    },
+    {
+        title: "Middle Eastern Falafel Bowl with Garlicky Tahini Sauce and Whole Grains",
+        id: 10,
+        ingredients: "Brown rice, lentils, quinoa, red bell pepper, tahini",
+        description: `Take your taste buds on a tour of the Middle East with this saucy, purely plant bowl.
+        It starts with an aromatic, protein-packed blend of lentils, quinoa, wild rice, and brown rice.
+        Layered with roasted red, yellow, and green bell peppers with onions.
+        The main event: golden, gluten-free falafel balls, seasoned with cumin, green onion, parsley, and cilantro.
+        Topping it all off is our punchy and garlicky lemon-tahini sauce.`,
+        nutrition: "Calories: 430. Carbs: 59g. Total fat: 17g. Protein: 11g.",
+        diet: "plant-based"
+    },
+    {
+        title: "Very Verde Chicken with Super Greens and Roaster Cauliflower",
+        id: 11,
+        ingredients: "Chicken breast, spinach, cauliflower, kale, swiss chard, parmesan cheese",
+        description: `Eating your greens never tasted (or felt) so good—this savory dish skips excess carbs for extra protein.
+        Lean, grilled chicken breast is slicked in our tasty pesto, supercharged with kale, basil, and cashew butter.
+        Garlicky super greens sauté of kale, spinach, escarole, tricolor swiss chard, and shredded parmesan.
+        A side of cauliflower, roasted with a pinch of cayenne, for a final veggie boost.`,
+        nutrition: "Calories: 470. Carbs: 11g. Total fat: 30g. Protein: 40g.",
+        diet: "regular"
+    },
+    {
+        title: "Chicken al Limone with Zoodles and Garlicky Broccoli",
+        id: 12,
+        ingredients: "Cashew butter, cassava flour, chicken breast, garlic, lemon, broccoli",
+        description: `Satisfy your pasta cravings without the excess carbs or dairy.
+        Our zoodles come slicked in a lemony piccata-style sauce made with garlic, cashew butter, and capers.
+        Juicy, diced chicken breast, coated in cassava flour and oven-baked for a greaseless finish.
+        Mixed with garlicky blanched broccoli, for even more good-for-you greens.`,
+        nutrition: "Calories: 300. Carbs: 16g. Total fat: 16g. Protein: 25g.",
+        diet: "low-calorie"
+    },
+    {
+        title: "Baked Turkey Meatballs with Zoodles and Eight-Veggie Ragu",
+        id: 13,
+        ingredients: "Ground turkey, marinara sauce, mushrooms, parmesan cheese, zucchini",
+        description: `Carb-conscious meets extra cozy in our harvest-happy reboot of spaghetti & meatballs, with spiralized zucchini in place of heavy noodles.
+        Hearty veggie ragù with a who’s who of nutritious greens, reds, and oranges, such as zucchini, butternut squash, kale, and tomato.
+        Juicy turkey-mushroom meatballs topped with grated parm.
+        On the side: an aromatic medley of fennel, onion, and bell peppers.`,
+        nutrition: "Calories: 360. Carbs: 20g. Total fat: 20g. Protein: 32g.",
+        diet: "low-calorie"
+    },
+    {
+        title: "Oven-baked Chicken Florentine with Creamed Spinach and Riced Broccoli Pilaf",
+        id: 14,
+        ingredients: "Broccoli, cauliflower, chicken breast, honey, mozzarella cheese, spinach",
+        description: `High-protein chicken breast is coated in tasty spices, cassava flour, and parmesan, then oven-baked to golden perfection.
+        Classic creamed spinach serves up lip-smacking flavor + vitamin A.
+        Our grain-free “pilaf” of riced broccoli and cauliflower, tossed with green olives, roasted bell peppers, and Mediterranean spices, swaps extra carbs from rice for fiberful veg.
+        With a final sprinkling of melty mozzarella cheese.`,
+        nutrition: "Calories: 410. Carbs: 16g. Total fat: 22g. Protein: 39g.",
+        diet: "regular"
+    },
+    {
+        title: "Super Kale Peste Chicken with Roasted Seasonal Veggies",
+        id: 15,
+        ingredients: "Broccoli, carrots, chicken breast, kale, olive oil, pecans",
+        description: `High-protein chicken breast grilled with thyme.
+        Plant-powered pesto, blended with a superfood mix of basil, kale, and cashew butter + pumpkin, flax, and hemp seeds.
+        Served over a seasonal mix of roasted butternut squash, broccoli, mushrooms, and leeks.
+        Topped with toasted pecans for a naturally sweet & nutty touch.
+        With lean protein from chicken and gut-friendly fiber from veggies, this dairy-free meal is feel-good food at its finest.`,
+        nutrition: "Calories: 400. Carbs: 19g. Total fat: 21g. Protein: 37g.",
+        diet: "low-calorie"
+    },
+    {
+        title: "Indian-Spiced Chickpea Curry Bowl with Basmati Rice, Lentils, and Veggies",
+        id: 16,
+        ingredients: "Cauliflower, curry powder, green chickpeas, lentils, peas, tomatoes",
+        description: `A totally plant-powered, Indian-inspired curry with coconutty creaminess and a rich golden glow.
+        Mildly spiced with turmeric, ginger, coriander, garam masala, cilantro, and lime.
+        Clean plant protein from chickpeas and brown lentils, and absolutely no faux anything.
+        Cauliflower, tomatoes, celery, and potatoes add a hearty bite.
+        Served over fluffy and fragrant basmati rice.`,
+        nutrition: "Calories: 360. Carbs: 50g. Total fat: 12g. Protein: 11g.",
+        diet: "plant-based, low-calorie"
+    },
+    {
+        title: "Glazed Parisian Carrots with Apple, Honey and Cinnamon",
+        id: 17,
+        ingredients: "Sherry vinegar, vegetable stock, apple, carrots, honey, orange juice",
+        description: `Our multi-serve side of oven-roasted Parisian carrots bring fall flavors to any feast, with a honeyed glaze of cinnamon, allspice, sherry vinegar, and diced apples. All that’s left is to heat & savor!
+        Quick & delicious hacks for mealtime magic:
+        Split it: with your nearest and dearest for an effortless side.
+        Mash it: with a pinch of cayenne and a sprinkle of salt for a textural twist.
+        Fritter it: with cheese, flour, egg, and seasonings; pan fry until golden.
+        Bake it: with ground ginger, walnuts, and muffin mix for the perfect carrot-y treat.`,
+        nutrition: "Calories: 90. Carbs: 16g. Total fat: 3g. Protein: 1g.",
+        diet: "low-calorie, plant-based"
+    },
+    {
+        title: "Unwrapped Salsa Verde Burrito with Purely Plant Crumbles",
+        id: 18,
+        ingredients: "Black beans, cilantro, pinto beans, plant-based crumbles, sweet potatoes, tomatillo",
+        description: `The best of a burrito, minus the the tortilla.
+        Our signature, purely plant protein crumbles are simply made of yellow peas, mushrooms, pinto beans, and fragrant spices, for a whopping 10g of sunny plant protein.
+        Mixed with roasted sweet potato, carrots, bell peppers, and a blend of black and pinto beans.
+        A salsa verde of cilantro, tomatillo, and green chile adds a fiery green kick.`,
+        nutrition: "Calories: 400. Carbs: 58g. Total fat: 15g. Protein: 11g.",
+        diet: "low-calorie, plant-based"
+    },
+    {
+        title: "Farmstead Baked Lentil Pasta with Seasonal Veggies and Creamy Marinara",
+        id: 19,
+        ingredients: "Broccoli, Butternut squash, green chickpeas, marinara sauce, mushrooms",
+        description: `The full bounty of plant-based eating, in one plate of baked pasta.
+        Starring hearty, gluten-free macaroni made of brown rice and lentils.
+        Tossed through with tons of vibrant veg (think: carrots, butternut squash, green chickpeas, broccoli, and mushrooms.
+        Baked with our dreamy, dairy-free, coconut milk marinara sauce made with creamy cashew butter.`,
+        nutrition: "Calories: 500. Carbs: 62g. Total fat: 23g. Protein: 12g.",
+        diet: "plant-based"
+    },
+    {
+        title: `Buffalo Cauliflower Mac and "Cheeze" with Garlicky Broccoli`,
+        id: 20,
+        ingredients: "Broccoli, buffalo sauce, butternut squash, cashews, nutritional yeast",
+        description: `A plant-powered mac so “cheezy,” you won’t even notice it’s dairy-free.
+        Starring hearty, gluten-free macaroni made of brown rice and lentils.
+        All coated in a smooth “cheeze" sauce of nutritional yeast, cashew butter, coconut milk, and butternut squash.
+        Mixed through with tender cauliflower florets, oven-roasted in a tahini-buffalo sauce.
+        Bright, garlicky broccoli to the side.`,
+        nutrition: "Calories: 430. Carbs: 43g. Total fat: 25g. Protein: 12g.",
+        diet: "plant-based"
+    }
+]
