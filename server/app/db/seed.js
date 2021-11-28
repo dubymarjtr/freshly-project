3c7f17c67a395f40e446892d377444d5358dc7e9
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-11-28 12:13:15 -0600

    feat: :sparkles: seed the database
    clear db before seeding

--- server/app/db/seed.js
+++ server/app/db/seed.js
@@ -1,3 +1,11 @@
+import once from "./conns/once.js";
 import meals from "./data.js";
 
-console.log(meals);
+(async () => {
+    const conn = await once.connect();
+    await conn.db("freshly").collection("meals").deleteMany({});
+    await conn.db("freshly").collection("meals").insertMany(meals);
+    console.log("data seeded")
+
+    conn.close();
+})();
