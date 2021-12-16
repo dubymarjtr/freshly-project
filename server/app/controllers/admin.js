72651d67913de803bf6f4c8fb083f62c6c7848ba
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-12-15 21:50:52 -0600

    generate JWT when creating admin

--- server/app/controllers/admin.js
+++ server/app/controllers/admin.js
@@ -1,6 +1,7 @@
 import client from "../db/conns/client.js";
 import config from "../config.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const admin = client.db(config.db.name).collection("admin");
 
@@ -19,5 +20,25 @@ export default {
 
     // Create admin and insert into database
     return admin.insertOne({ username, password: hashedPassword });
+  },
+
+  async show(username, password) {
+    const adminUser = await admin.findOne({ username });
+
+    if (!adminUser) {
+      throw new Error("Login unsuccessful");
+    }
+
+    // Check if password is correct
+    const match = await bcrypt.compare(password, adminUser.password);
+
+    if (!match) {
+      throw new Error("Login unsuccessful");
+    }
+
+    // Create JWT
+    return jwt.sign({ username }, config.encryption.secret, {
+      expiresIn: config.encryption.expiresIn,
+    });
   },
 };
--- server/app/routes/admin.js
+++ server/app/routes/admin.js
@@ -11,8 +11,12 @@ router.get("/", (_, res) => {
 router.post("/register", async (req, res) => {
   try {
     const { username, password } = req.body;
-    const newAdmin = await adminController.create(username, password);
-    res.json(newAdmin);
+    // create admin using controller
+    await adminController.create(username, password);
+
+    // save JWT after registering admin
+    const token = await adminController.show(username, password);
+    res.send(token);
   } catch ({ message }) {
     res.status(400).json(message);
   }
