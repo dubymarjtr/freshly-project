e2b40d6e81251398394413dd368909151a38140f
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-12-15 22:54:53 -0600

    perf: :white_check_mark: get all meals (protected)

--- server/app/routes/meals.js
+++ server/app/routes/meals.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import mealsController from "../controllers/meals.js";
+import isAuth from "../middleware/isAuth.js";
 
 const router = new Router();
 
@@ -7,10 +8,13 @@ router.get("/", (_, res) => {
   res.send("Take a look at our Freshly Cooked Meals!");
 });
 
-router.post("/", async (_, res) => {
-  // Get all meals from controller
-  const meals = await mealsController.index();
-  res.json(meals);
+router.post("/", async ({ isAuth }, res) => {
+  if (isAuth) {
+    const meals = await mealsController.index();
+    res.json(meals);
+  } else {
+    res.status(401).json({ message: "Access Denied" });
+  }
 });
 
 export default router;
