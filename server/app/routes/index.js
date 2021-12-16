b1889b53275386b06a7d06113e8f1864f7b94990
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-12-16 16:05:01 -0600

    import and use customer route

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import adminRouter from "./admin.js";
 import mealsRouter from "./meals.js";
+import customerRouter from "./customer.js";
 
 const router = new Router();
 
@@ -10,5 +11,6 @@ router.get("/", (_, res) => {
 
 router.use("/admin", adminRouter);
 router.use("/meals", mealsRouter);
+router.use("/customer", customerRouter);
 
 export default router;
