b8aee3d986edbe13c6c1e1d8ef213121be851f67
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-12-15 23:25:57 -0600

    perf: :zap: get meals with optional filters
    protected

--- server/app/controllers/meals.js
+++ server/app/controllers/meals.js
@@ -5,7 +5,30 @@ const meals = client.db(config.db.name).collection("meals");
 
 export default {
   // Get all meals
-  index() {
-    return meals.find({}).toArray();
+  async index(req) {
+    const keys = Object.keys(req.query);
+    const values = Object.values(req.query);
+
+    const mealsData = await meals.find({}).toArray();
+
+    function filterMeals(meals, keys, values) {
+      let index;
+      if (keys.includes("keywords")) {
+        index = keys.indexOf("keywords");
+        meals = meals.filter(
+          (meal) =>
+            meal.title.includes(values[index]) ||
+            meal.description.includes(values[index]) ||
+            meal.ingredients.includes(values[index])
+        );
+      }
+      if (keys.includes("diet-type")) {
+        index = keys.indexOf("diet-type");
+        meals = meals.filter((meal) => meal.diet.includes(values[index]));
+      }
+      return meals;
+    }
+
+    return filterMeals(mealsData, keys, values);
   },
 };
