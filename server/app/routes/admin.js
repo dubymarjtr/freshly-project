4921f297b75c1f88de541db5bac05a42988a63b4
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-12-16 00:43:17 -0600

    refactor: :recycle: import Admin model and create new instance
    pass admin instance (includes username/password)

--- server/app/routes/admin.js
+++ server/app/routes/admin.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import adminController from "../controllers/admin.js";
+import Admin from "../models/users/Admin.js";
 
 const router = new Router();
 
@@ -10,12 +11,13 @@ router.get("/", (_, res) => {
 // Create admin
 router.post("/register", async (req, res) => {
   try {
-    const { username, password } = req.body;
+    // create admin instance
+    const admin = new Admin(req.body);
     // create admin using controller
-    await adminController.create(username, password);
+    await adminController.create(admin);
 
     // save JWT after registering admin
-    const token = await adminController.show(username, password);
+    const token = await adminController.show(admin);
     res.send(token);
   } catch ({ message }) {
     res.status(400).json(message);
